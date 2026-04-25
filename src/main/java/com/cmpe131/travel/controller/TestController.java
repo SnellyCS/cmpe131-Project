77ecdfb36db80ad2d230ed3c1bd663c887f283ca
Dylan <dylansnellstrom@gmail.com>
2026-04-25 11:40:38 -0700

    Set up Spring Boot backend with SQLite connection

--- src/main/java/com/cmpe131/travel/controller/TestController.java
+++ src/main/java/com/cmpe131/travel/controller/TestController.java
@@ -1,18 +1,25 @@
 package com.cmpe131.travel.controller;
 
+import com.cmpe131.travel.repository.BookingRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Travel API is running";
+    private final BookingRepository bookingRepository;
+
+    public TestController(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
     }
 
-    @GetMapping("/test")
-    public String test() {
-        return "Test endpoint works";
+    @GetMapping("/db-test")
+    public String testDB() {
+        return "DB result: " + bookingRepository.testConnection();
+    }
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return bookingRepository.getAllUsers();
     }
 }
--- src/main/java/com/cmpe131/travel/repository/BookingRepository.java
+++ src/main/java/com/cmpe131/travel/repository/BookingRepository.java
@@ -12,6 +12,17 @@ public class BookingRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    public int testConnection() {
+        return jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+    }
+
+    public String getAllUsers() {
+    return jdbcTemplate.query(
+        "SELECT First_Name, Last_Name FROM Users",
+        (rs, rowNum) -> rs.getString("First_Name") + " " + rs.getString("Last_Name")
+    ).toString();
+    }
+
     // TODO: Write SQL queries for Bookings table
 
     // TODO: Write SQL queries for Hotel_Reservations table
