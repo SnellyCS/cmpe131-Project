2c2a7ab9ea03aaff54647df29598b5e7f3bdebca
Eslin <villaltaeslin96@gmail.com>
2026-04-27 19:45:50 -0700

    Added FlightController endpoint

--- src/main/java/com/cmpe131/travel/controller/FlightController.java
+++ src/main/java/com/cmpe131/travel/controller/FlightController.java
@@ -1,15 +1,30 @@
 package com.cmpe131.travel.controller;
 
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/v1/flights")
 public class FlightController {
 
-    // TODO: Search flights using external API
-    // Example: SFO -> JFK, depart date, return date
     @GetMapping("/search")
-    public String searchFlights() {
-        return "TODO: flight search endpoint";
+    public List<Map<String, Object>> searchFlights(
+            @RequestParam String origin,
+            @RequestParam String destination,
+            @RequestParam String departDate
+    ) {
+        List<Map<String, Object>> flights = new ArrayList<>();
+
+        Map<String, Object> flight1 = new HashMap<>();
+        flight1.put("airline", "United");
+        flight1.put("flightNumber", "UA123");
+        flight1.put("origin", origin);
+        flight1.put("destination", destination);
+        flight1.put("departDate", departDate);
+        flight1.put("price", 250);
+
+        flights.add(flight1);
+
+        return flights;
     }
 }
